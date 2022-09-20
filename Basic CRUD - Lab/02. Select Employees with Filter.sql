a81af653a16f152679e40e6c3a91aada584da931
Boris Mitov <101501574+Bohera@users.noreply.github.com>
2022-09-20 21:11:50 +0300

    Delete Basic CRUD - Lab directory

--- Basic CRUD - Lab/02. Select Employees with Filter.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-	`id`,
-	CONCAT_WS(' ', `first_name` , `last_name`) AS 'full_name',
-    `job_title` AS 'Job title',
-	`salary` AS 'Salary'
-	FROM `employees` 
-    WHERE `salary` > 1000
-	ORDER BY `id`;
